696fb6376223b2d6b490eddfeb821718beee2b72
dotnet-code <@dotnet-code>
2023-09-29 12:32:51 +0000

    commit from code202309293251

---
+++ Angular/src/app/payment/payment.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.css']
+})
+export class PaymentComponent implements OnInit {
+  paymentForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.paymentForm = this.formBuilder.group({
+      cardNumber: ['', [Validators.required, Validators.pattern('^[0-9]{16}$')]],
+      cvv: ['', [Validators.required, Validators.pattern('^[0-9]{3}$')]],
+      amount: ['', Validators.required],
+      currency: ['', Validators.required]
+    });
+  }
+
+  submitForm() {
+    if (this.paymentForm.invalid) {
+      return;
+    }
+
+    // Perform API call to securely transfer and convert data
+    // You can access form values using this.paymentForm.value
+  }
+}
